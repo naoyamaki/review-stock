eaae3a72866e7747b879e7d7b17b368ed14cc986
naoyamaki <stringbassandsticks@gmail.com>
2023-04-28 00:20:07 +0900

    一応動確できる程度に実装

--- back-src/build.gradle.kts
+++ back-src/build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	war
-	id("org.springframework.boot") version "3.0.3-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+  war
+  id("org.springframework.boot") version "3.0.3-SNAPSHOT"
+  id("io.spring.dependency-management") version "1.1.0"
+  kotlin("jvm") version "1.7.22"
+  kotlin("plugin.spring") version "1.7.22"
 }
 
 group = "com.example"
@@ -13,26 +13,34 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
-	maven { url = uri("https://repo.spring.io/snapshot") }
+  mavenCentral()
+  maven { url = uri("https://repo.spring.io/milestone") }
+  maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation("org.jetbrains.exposed", "exposed-core", "0.39.2")
+  implementation("org.jetbrains.exposed", "exposed-dao", "0.39.2")
+  implementation("org.jetbrains.exposed", "exposed-jdbc", "0.39.2")
+  implementation("mysql:mysql-connector-java:8.0.30")
+  providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+  kotlinOptions {
+    freeCompilerArgs = listOf("-Xjsr305=strict")
+    jvmTarget = "17"
+  }
+}
+tasks.withType<War> {
+  enabled = true
+  archiveName = "ROOT.war"
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+  useJUnitPlatform()
 }
