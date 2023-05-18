b39ece7101e5e4d0a1e16076221b5b79a1788946
naoyamaki <stringbassandsticks@gmail.com>
2023-05-18 18:17:45 +0900

    cloudfrontと静的資材バケットを作成

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { InfraStack } from '../lib/infra-stack';
+import { ApplicationStack } from '../lib/application-stack';
 
 const app = new cdk.App();
 
@@ -9,5 +10,9 @@ const app = new cdk.App();
 const context = app.node.tryGetContext('environment');
 
 new InfraStack(app, 'InfraStack', {
+  env: {account: context.env}
+});
+
+new ApplicationStack(app, 'ApplicationStack', {
   env: {account: context.env}
 });
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,14 +1,12 @@
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as cdk from 'aws-cdk-lib';
+import { aws_ec2, aws_s3, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
-export class InfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class InfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     const context = this.node.tryGetContext('environment')
 
-    const vpc = new ec2.Vpc(this, 'VPC', {
+    const vpc = new aws_ec2.Vpc(this, 'VPC', {
       availabilityZones: ['ap-northeast-1c', 'ap-northeast-1d'],
       ipAddresses: context.vpcCidr,
       vpcName: context.serviceName,
@@ -17,57 +15,57 @@ export class InfraStack extends cdk.Stack {
       subnetConfiguration: [{
         cidrMask: 25,
         name: 'public-1c',
-        subnetType: ec2.SubnetType.PUBLIC,
+        subnetType: aws_ec2.SubnetType.PUBLIC,
       },{
         cidrMask: 25,
         name: 'public-1d',
-        subnetType: ec2.SubnetType.PUBLIC,
+        subnetType: aws_ec2.SubnetType.PUBLIC,
       },{
         cidrMask: 25,
         name: 'private-app-1c',
-        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        subnetType: aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
       },{
         cidrMask: 25,
         name: 'private-app-1d',
-        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        subnetType: aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
       },{
         cidrMask: 25,
         name: 'private-db-1c',
-        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        subnetType: aws_ec2.SubnetType.PRIVATE_ISOLATED,
       },{
         cidrMask: 25,
         name: 'private-db-1d',
-        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        subnetType: aws_ec2.SubnetType.PRIVATE_ISOLATED,
       }]
     });
 
     // サブネットの設定を実装
-    const public1c = vpc.publicSubnets[0].node.defaultChild as ec2.CfnSubnet
+    const public1c = vpc.publicSubnets[0].node.defaultChild as aws_ec2.CfnSubnet
     public1c.addPropertyOverride('CidrBlock', context.pubricCCider)
     public1c.addPropertyOverride('availabilityZone', 'ap-northeast-1c')
-    const public1d = vpc.publicSubnets[1].node.defaultChild as ec2.CfnSubnet
+    const public1d = vpc.publicSubnets[1].node.defaultChild as aws_ec2.CfnSubnet
     public1d.addPropertyOverride('CidrBlock', context.publicDCider)
     public1d.addPropertyOverride('availabilityZone', 'ap-northeast-1d')
-    const privateApp1c = vpc.privateSubnets[0].node.defaultChild as ec2.CfnSubnet
+    const privateApp1c = vpc.privateSubnets[0].node.defaultChild as aws_ec2.CfnSubnet
     privateApp1c.addPropertyOverride('CidrBlock', context.privateAppCCider)
     privateApp1c.addPropertyOverride('availabilityZone', 'ap-northeast-1c')
-    const privateApp1d = vpc.privateSubnets[1].node.defaultChild as ec2.CfnSubnet
+    const privateApp1d = vpc.privateSubnets[1].node.defaultChild as aws_ec2.CfnSubnet
     privateApp1d.addPropertyOverride('CidrBlock', context.privateAppDCider)
     privateApp1d.addPropertyOverride('availabilityZone', 'ap-northeast-1d')
-    const privateDb1c = vpc.privateSubnets[2].node.defaultChild as ec2.CfnSubnet
+    const privateDb1c = vpc.privateSubnets[2].node.defaultChild as aws_ec2.CfnSubnet
     privateDb1c.addPropertyOverride('CidrBlock', context.privateDbCCider)
     privateDb1c.addPropertyOverride('availabilityZone', 'ap-northeast-1c')
-    const privateDb1d = vpc.privateSubnets[3].node.defaultChild as ec2.CfnSubnet
+    const privateDb1d = vpc.privateSubnets[3].node.defaultChild as aws_ec2.CfnSubnet
     privateDb1d.addPropertyOverride('CidrBlock', context.privateDbDCider)
     privateDb1d.addPropertyOverride('availabilityZone', 'ap-northeast-1d')
 
     // create VPC flow log
-    const flowLogBucket = new s3.Bucket(this, 'Bucket', {
+    const flowLogBucket = new aws_s3.Bucket(this, 'Bucket', {
       bucketName: context.serviceName+"-vpc-flow-log"
     });
-    const flowLog = new ec2.FlowLog(this, 'FlowLog', {
-      resourceType: ec2.FlowLogResourceType.fromVpc(vpc),
-      destination: ec2.FlowLogDestination.toS3(flowLogBucket)
+    const flowLog = new aws_ec2.FlowLog(this, 'FlowLog', {
+      resourceType: aws_ec2.FlowLogResourceType.fromVpc(vpc),
+      destination: aws_ec2.FlowLogDestination.toS3(flowLogBucket)
     });
 
   }
